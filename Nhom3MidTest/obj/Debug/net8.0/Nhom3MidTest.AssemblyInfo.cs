ee83cf746973f9e5816ee2c5ab95fb74effaa9f4
Celenia <lunanguyen.2102003@gmail.com>
2024-08-09 16:27:19 +0700

    cau3

--- Nhom3MidTest/obj/Debug/net8.0/Nhom3MidTest.AssemblyInfo.cs
+++ Nhom3MidTest/obj/Debug/net8.0/Nhom3MidTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nhom3MidTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a50fac1d7ecea415bbc956766f3049c8c34b38a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39c71bacb1502b5a73a40b4e4411079e39cc038d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nhom3MidTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nhom3MidTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
