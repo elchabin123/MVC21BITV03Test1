da6aaa7f009870a6b6d2369887fad13a2e988b9c
elchabin123 <nghbvinh19@gmail.com>
2024-08-09 13:56:23 +0700

    commit before merge

--- Nhom3MidTest/obj/Debug/net8.0/Nhom3MidTest.AssemblyInfo.cs
+++ Nhom3MidTest/obj/Debug/net8.0/Nhom3MidTest.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Nhom3MidTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a048a57723a91220fe52ceeeaa85bd2ec820f07d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bfdaa7852b415a701edee91bd8376ccdce15a6e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Nhom3MidTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Nhom3MidTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
